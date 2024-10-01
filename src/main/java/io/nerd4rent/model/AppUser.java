41d8f6f482a73177f7c9fe2967cf923f156cb9fb
Paweł Wlazło <pawel@wlazlo.dev>
2024-10-01 19:04:10 +0200

    dodanie rejestracji użytkowników

--- src/main/java/io/nerd4rent/model/AppUser.java
+++ src/main/java/io/nerd4rent/model/AppUser.java
@@ -14,5 +14,18 @@ public class AppUser {
     private String email;
     private String password;
 
-    // Getters, setters, constructor, toString
+    // Gettery i settery
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // ... inne gettery i settery ...
 }
